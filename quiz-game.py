0e5a26ac11e48ddceb2af9699a3021ec875ef313
Elliot C. Alexandre <elliot.c.alexandre@proton.me>
2026-07-11 20:32:44 -0400

    Specified the variable types

--- quiz-game.py
+++ quiz-game.py
@@ -1,4 +1,4 @@
-quiz_running = True; question_1 = True
+quiz_running : bool = True ; question_1 :bool = True
 question_2 = question_3 = question_4 = question_5 = question_6 = question_7 = question_8 = question_9 = question_10 = question_11 = False
 question_12 = question_13 = question_14 = question_15 = BONUS_question = False
 score = good_answers_gotten_by_user = 0
